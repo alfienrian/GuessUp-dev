1dfb163df9affa037e66d28e61830e9fe8502004
Amro Alfien <55046247+alfienrian@users.noreply.github.com>
2024-05-27 11:19:13 +0700

    QuizData: penambahan method untuk mengacak 5 soal

--- app/src/main/java/com/ti2a/kel3/guessup/data/QuizData.kt
+++ app/src/main/java/com/ti2a/kel3/guessup/data/QuizData.kt
@@ -11,16 +11,38 @@ object QuizRepository {
     val animalQuizList = listOf(
         QuizItem(R.drawable.cat, "kucing"),
         QuizItem(R.drawable.belalang, "belalang"),
-        // TODO: Mengganti semua data gambar
         QuizItem(R.drawable.badak, "badak"),
         QuizItem(R.drawable.bebek, "bebek"),
-//        QuizItem(R.drawable.cat, "harimau"),
         QuizItem(R.drawable.buaya, "buaya"),
         QuizItem(R.drawable.gajah, "gajah"),
+        QuizItem(R.drawable.anjing, "anjing"),
+        QuizItem(R.drawable.ayam, "ayam"),
+        QuizItem(R.drawable.ikan, "ikan"),
+        QuizItem(R.drawable.kambing, "kambing"),
+        QuizItem(R.drawable.kelinci, "kelinci"),
+        QuizItem(R.drawable.jerapah, "jerapah"),
+        QuizItem(R.drawable.kuda, "kuda"),
+        QuizItem(R.drawable.kumbang, "kumbang"),
+        QuizItem(R.drawable.singa, "singa"),
+        QuizItem(R.drawable.penyu, "penyu"),
+        QuizItem(R.drawable.zebra, "zebra"),
+        QuizItem(R.drawable.panda, "panda"),
+        QuizItem(R.drawable.sapi, "sapi"),
+        QuizItem(R.drawable.sipuuut, "siput"),
+        QuizItem(R.drawable.snake, "ular"),
+
     )
 
     val fruitQuizList = listOf(
         QuizItem(R.drawable.avocado, "alpukat"),
         // Tambahkan soal tebakan buah lainnya
     )
+
+    fun getRandomAnimals(): List<QuizItem> {
+        return animalQuizList.shuffled().take(5)
+    }
+
+    fun getRandomFruits(): List<QuizItem> {
+        return fruitQuizList.shuffled().take(5)
+    }
 }
